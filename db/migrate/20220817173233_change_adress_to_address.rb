9c9e4001a33df78c9dfd3cb239ac9b7395f3bc4a
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <lucas.mturra@gmail.com>
2022-08-17 17:08:05 -0300

    resolvido

---
+++ db/migrate/20220817173233_change_adress_to_address.rb
@@ -0,0 +1,5 @@
+class ChangeAdressToAddress < ActiveRecord::Migration[7.0]
+  def change
+    rename_column(:restaurants, :adress, :address)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Creating seeds"
+
+restaurant_one = Restaurant.create(name: "Cão Veio", address: "Pinheiros", phone_number: "01234 578990", category: "chinese")
+restaurant_two = Restaurant.create(name: "Grano", address: "Mogi das Cruzes", phone_number: "01234 840200", category: "italian")
+restaurant_three = Restaurant.create(name: "Patties", address: "Itam Bibi", phone_number: "01234 283648", category: "japanese")
+restaurant_four = Restaurant.create(name: "Barato Total", address: "Brás Cubas", phone_number: "01234 627394", category: "french")
+restaurant_five = Restaurant.create(name: "Braz Elettrica", address: "Madalena", phone_number: "01234 987123", category: "belgian")
+
+puts "Seeds created!"
